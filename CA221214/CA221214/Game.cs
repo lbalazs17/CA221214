fe19b7c2a7d46a6c2408b2eb82a439269d6ea944
Lakatos Balázs <72004742+lbalazs17@users.noreply.github.com>
2022-12-14 09:33:25 +0100

    Add files via upload

---
+++ CA221214/CA221214/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA221214
+{
+    internal class Game
+    {
+        public int ReleaseYear { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public string[] Platforms { get; set; }
+
+        public Game(string row)
+        {
+            string[] v = row.Split(';');
+            ReleaseYear = int.Parse(v[0]);
+            Title = v[1];
+            Genre = v[2];
+            Publisher = v[3];
+            Platforms = v[4].Split(", ");
+        }
+    }
+}
---
+++ CA221214/CA221214/Program.cs
@@ -0,0 +1,49 @@
+﻿using CA221214;
+List <Game> games = new();
+using StreamReader sr = new("..\\..\\..\\res\\bestgames.csv");
+Random rnd = new Random(); 
+while (!sr.EndOfStream)
+{
+    games.Add(new Game(sr.ReadLine()));
+}
+Console.WriteLine($"f1: Összesen {games.Count} játék szerepel a listában!");
+
+Console.WriteLine("f2: ezekben az években került a legtöbb játék listára:");
+var grp = games.GroupBy(x => x.ReleaseYear)
+    .Where(x => x.Count() > 10)
+    .OrderByDescending(x => x.Count());
+foreach (var ytg in grp)
+{
+    Console.WriteLine($"\t{ytg.Key}: {ytg.Count()}db");
+}
+var fpss = games.Where(x => x.Genre == "First-person shooter").ToList();
+Console.WriteLine($"f3: összesen {fpss.Count} FPS került a listára! + " +
+    $"\n\tpéldául: {fpss[rnd.Next(fpss.Count)].Title}");
+//Dictionary<int, int> dic = new();
+//foreach (Game g in games)
+//{
+//    if (!dic.ContainsKey(g.ReleaseYear))
+//    {
+//        dic.Add(g.ReleaseYear, 1);
+//    }
+//    else dic[g.ReleaseYear]++;
+//}
+//var kvpl = dic.ToList();
+//for (int i = 0; i < kvpl.Count-1; i++)
+//{
+//    for (int j = i+1; j < kvpl.Count; j++)
+//    {
+//        if (kvpl[j].Value > kvpl[i].Value)
+//        {
+//            kvpl[i],kvpl[j] = (kvpl[j]);
+
+//        }
+//    }
+//}
+//foreach (var kvp in kvpl)
+//{
+//    if (kvp.Value>10)
+//    {
+//        Console.WriteLine($"{kvp.Key}: {kvp.Value}db");
+//    }
+//}
